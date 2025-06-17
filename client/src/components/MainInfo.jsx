ee74a39ff09506de8a7f69345ef1f96ec778f054
afriano <andresfeliperianom@gmail.com>
2025-06-16 21:02:08 -0600

    set the dynamic data in the main area, based in ip location

--- client/src/components/MainInfo.jsx
+++ client/src/components/MainInfo.jsx
@@ -1,22 +1,41 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 import { FaLocationDot } from "react-icons/fa6";
+import axios from 'axios';
+
+const apiUrl = import.meta.env.VITE_API_URL;
 
 function MainInfo() {
-  return (
+    const [weather, setWeather] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${apiUrl}`); // replace with your ngrok URL
+                console.log('Backend response:', response.data);
+                setWeather(response.data);
+            } catch (error) {
+                console.error('Error fetching weather data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
     <div className='flex flex-col p-4 h-full'>
         <div className='flex gap-2 items-center text-white'>
             <FaLocationDot />
-            Mexico city
+            {weather && weather.location ? weather.location.name : 'Loading...'}
         </div>
         <div className='flex justify-between mt-auto items-center'>
             <div>
-                <div className='text-4xl text-white'>Partly cloudy</div>
+                <div className='text-4xl text-white'>{weather ? weather.current.condition.text : 'Loading...'}</div>
                 <div className='flex gap-6 text-white'>
-                    <div>Tuesday, april 24</div>
-                    <div>7:00 pm</div>
+                    <div>{weather ? new Date(weather.location.localtime).toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' }) : 'Loading...'}</div>
+                    <div>{weather ? new Date(weather.location.localtime).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' }) : 'Loading...'}</div>
                 </div>
             </div>
-            <div className='text-4xl text-white'>27°C</div>
+            <div className='text-4xl text-white'>{weather ? weather.current.temp_c + '°C' : 'Loading...'}</div>
         </div>
     </div>
   )
