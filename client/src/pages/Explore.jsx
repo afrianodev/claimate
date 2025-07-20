3c3738b6ad3b785c4ebc8f2f01815c7228f1f9d2
afriano <andresfeliperianom@gmail.com>
2025-07-19 18:54:03 -0600

    Add under construction section
    Put the two under construction sections structure for next steps

--- client/src/pages/Explore.jsx
+++ client/src/pages/Explore.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import { LuConstruction } from "react-icons/lu";
 
 function Explore() {
   return (
-    <div className='w-[90%] h-[85%] rounded-lg shadow-md flex overflow-hidden bg-neutral-300/50 backdrop-blur-sm'>Explore</div>
+    <div className='w-[90%] h-[85%] rounded-lg shadow-md flex overflow-hidden bg-neutral-300/50 backdrop-blur-sm text-4xl items-center justify-center gap-8'>Section under construction <span className='text-8xl'><LuConstruction /></span></div>
   )
 }
 
